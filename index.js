31cd0176d11a73512fca573226ad2dc2cffde465
dbsasser <danielbsasser@gmail.com>
2020-05-30 11:36:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    let noCaps = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = [];
+    let stringRay = string.split(' ')
+
+    for (let i = 0; i < stringRay.length; i++) {
+      if (i === 0) {
+        result.push( this.capitalize(stringRay[i])) 
+      } else {
+        if (noCaps.includes(stringRay[i])) {
+          result.push(stringRay[i])
+        } else {
+          result.push(this.capitalize(stringRay[i]))
+        }
+      }
+    }
+
+    return result.join(" ");
+  }
 }
+
